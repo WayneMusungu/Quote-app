5dd2587899110e7f298bd3877aa144b4c08c3369
WayneMusungu <waynemuyera17@gmail.com>
2022-04-09 15:21:40 +0300

    Add content to quote.component.html and link it the child component to the parent component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,9 +13,8 @@ export class QuoteComponent implements OnInit {
     new Quotes(1, 'Otima', 'Skills', 'Learn a skill, master and monetize it. Work anywhere you want, how you want and with anyone you want. Small incremental steps.', 'Milton Berle', new Date(2022, 2, 2), 0, 0),
 
   ];
-  toggleDetails(index){
-    this.quotes[index].showInformation = !this.quotes[index].showInformation;
-  }
+
+  
   constructor() { }
 
   ngOnInit(): void {
