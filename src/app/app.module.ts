8a05ca40902709aaf43fbb1bb90c90ea78b26a48
WayneMusungu <waynemuyera17@gmail.com>
2022-04-09 13:38:05 +0300

    Add a new file of quote-detail that display description of the quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
+    QuoteDetailComponent,
   ],
   imports: [
     BrowserModule
