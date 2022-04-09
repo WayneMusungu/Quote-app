7b88943e07850040510a0b8f783f63edf7309296
WayneMusungu <waynemuyera17@gmail.com>
2022-04-09 13:27:34 +0300

    Add two new Quotes to quotes.component.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,14 @@ import { Quotes } from '../quotes';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quotes[] = [
-    new Quotes(1, 'Otima', 'Skills', 'Learn a skill, master and monetise it. Work anywhere you want, how you want and with anyone you want. Small incremental steps.', 'Milton Berle', new Date(2022, 2, 2), 0, 0),
+    new Quotes(1, 'Otima', 'Skills', 'Learn a skill, master and monetize it. Work anywhere you want, how you want and with anyone you want. Small incremental steps.', 'Milton Berle', new Date(2022, 2, 2), 0, 0),
+    new Quotes(1, 'Otima', 'Skills', 'Learn a skill, master and monetize it. Work anywhere you want, how you want and with anyone you want. Small incremental steps.', 'Milton Berle', new Date(2022, 2, 2), 0, 0),
+    new Quotes(1, 'Otima', 'Skills', 'Learn a skill, master and monetize it. Work anywhere you want, how you want and with anyone you want. Small incremental steps.', 'Milton Berle', new Date(2022, 2, 2), 0, 0),
 
   ];
-
+  toggleDetails(index){
+    this.quotes[index].showInformation = !this.quotes[index].showInformation;
+  }
   constructor() { }
 
   ngOnInit(): void {
