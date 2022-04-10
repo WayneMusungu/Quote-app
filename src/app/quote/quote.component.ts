951e1b455e151fdadd7e0beb81c5b105bab317d5
WayneMusungu <waynemuyera17@gmail.com>
2022-04-10 05:52:51 +0300

    Change stylings to cards

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -23,7 +23,7 @@ export class QuoteComponent implements OnInit {
 
   deleteQuote(isComplete, index){
     if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].title}?`)
 
       if (toDelete){
         this.quotes.splice(index,1)
