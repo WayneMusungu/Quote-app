23e8ea9e24e80f0a19ed6eea1cb06d4198d446da
WayneMusungu <waynemuyera17@gmail.com>
2022-04-11 06:46:02 +0300

    Auto-generated commit [ci skip]

--- src/app/quotes.ts
+++
@@ -1,9 +0,0 @@
-export class Quotes {
-    showInformation: boolean;
-    constructor(public id: number, public name: string, public title: string, public quote: string, public author:string, public datePosted: Date, public likes:number, public dislikes:number){
-        this.showInformation=false;
-    }
-} 
-
-
-    
